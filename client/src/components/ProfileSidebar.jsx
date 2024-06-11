8388e17a0c60b6fbce4a0646942241d9c7709513
Rubal <rubal103@gmail.com>
2024-06-11 00:10:40 -0400

    pull changes

--- client/src/components/ProfileSidebar.jsx
+++ client/src/components/ProfileSidebar.jsx
@@ -1,16 +1,33 @@
-import "./ProfileSidebar.css"
-import ProfileIcon from "../assets/profile-icon.png";
+// import { useParams } from 'react-router-dom';
+// import { useQuery } from '@apollo/client';
 
-export default function ProfileSidebar() {
+// import { QUERY_USER } from '../utils/queries';
+
+// import "./ProfileSidebar.css"
+// import ProfileIcon from "../assets/profile-icon.png";
+
+export default function ProfileSidebar () {
+    // const { userId } = useParams();
+
+    // const { loading, data } = useQuery(QUERY_USER, {
+    //   variables: { userId: userId },
+    // });
+    // const user = data?.user || {};
+
+    // if (loading) {
+    //   return <div>Loading...</div>;
+    // }
+
     return (
         <>
         <section className="profile-sidebar">
             <section className="profile-info">
                 <img src={ProfileIcon}/>
-                <h2> Username goes here </h2>
-                <p> Email: Email goes here</p>
+                <h2> {user.userName} </h2>
+                <p> {user.email} </p>
             </section>
         </section>
         </>
-    )
-}
+    );
+};
+
--- client/src/pages/User.jsx
+++ client/src/pages/User.jsx
@@ -1,13 +1,47 @@
+// import { useQuery } from '@apollo/client';
+
+// import { QUERY_USERS } from '../utils/queries';
+// import { Link } from 'react-router-dom';
 import ProfileSidebar from "../components/ProfileSidebar";
 import Reviews from "../components/Reviews";
+// import UserList from "../components/UserList";
 
 export default function User() {
-  return (
+//   const { loading, data } = useQuery(QUERY_USERS);
+// const users = data?.users || [];
+return (
+    
+//     <main>
+//      <div>
+// {loading ? (
+// <div>Loading...</div>
+//           ) : (
+        
+//             <UserList
+//             users = {users}
+            
+//           />        
+        
+//           )}  
+//            </div>  
+           
+          
+//     </main>
+    
+    
+    
+    
+    
+    
     <>
-      <section className="profile-container">
-        <ProfileSidebar />
-        <Reviews />
-      </section>
-    </>
-  )
-}
+  
+    <section className="profile-container"> 
+
+
+  <ProfileSidebar />
+  <Reviews />
+  {/* {/* <UserList  users = {users}/> */}
+  </section>
+  </>
+  );
+};
