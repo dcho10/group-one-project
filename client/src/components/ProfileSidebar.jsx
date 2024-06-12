78bfdb2ddd54d4f0e8a2f09552ee6fb591cf485c
Rubal <rubal103@gmail.com>
2024-06-12 09:34:39 -0400

    removed unused code

--- client/src/components/ProfileSidebar.jsx
+++ client/src/components/ProfileSidebar.jsx
@@ -1,24 +1,8 @@
-// import { useParams } from 'react-router-dom';
-// import { useQuery } from '@apollo/client';
-
-// import { QUERY_USER } from '../utils/queries';
-
-// import "./ProfileSidebar.css"
-// import ProfileIcon from "../assets/profile-icon.png";
+import "./ProfileSidebar.css"
+import ProfileIcon from "../assets/profile-icon.png";
 
 export default function ProfileSidebar () {
-    // const { userId } = useParams();
-
-    // const { loading, data } = useQuery(QUERY_USER, {
-    //   variables: { userId: userId },
-    // });
-    // const user = data?.user || {};
-
-    // if (loading) {
-    //   return <div>Loading...</div>;
-    // }
-
-    return (
+     return (
         <>
         <section className="profile-sidebar">
             <section className="profile-info">
