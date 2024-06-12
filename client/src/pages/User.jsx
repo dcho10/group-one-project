36c98ae4273f3fba86ca824fca093f51280f7d06
Rubal <rubal103@gmail.com>
2024-06-12 09:38:53 -0400

    removed unused code

--- client/src/pages/User.jsx
+++ client/src/pages/User.jsx
@@ -1,46 +1,14 @@
-// import { useQuery } from '@apollo/client';
-
-// import { QUERY_USERS } from '../utils/queries';
-// import { Link } from 'react-router-dom';
 import ProfileSidebar from "../components/ProfileSidebar";
 import Reviews from "../components/Reviews";
-// import UserList from "../components/UserList";
 
 export default function User() {
-//   const { loading, data } = useQuery(QUERY_USERS);
-// const users = data?.users || [];
+
 return (
-    
-//     <main>
-//      <div>
-// {loading ? (
-// <div>Loading...</div>
-//           ) : (
-        
-//             <UserList
-//             users = {users}
-            
-//           />        
-        
-//           )}  
-//            </div>  
-           
-          
-//     </main>
-    
-    
-    
-    
-    
-    
     <>
-  
-    <section className="profile-container"> 
-
-
+  <section className="profile-container"> 
   <ProfileSidebar />
   <Reviews />
-  {/* {/* <UserList  users = {users}/> */}
+ 
   </section>
   </>
   );
