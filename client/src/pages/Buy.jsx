b07e90cf4b8a0a61e8f23853174844bcd3a72072
ashok parihar <aparihar@email.arizona.edu>
2024-06-11 21:36:04 -0600

    Making corrections and adding data.

--- client/src/pages/Buy.jsx
+++ client/src/pages/Buy.jsx
@@ -1,7 +1,12 @@
 import BuyingCard from "../components/BuyingCard";
 import Message from "../components/Message";
+import {useQuery} from "@apollo/client";
+import { QUERY_USERS } from "../utils/queries";
 
 export default function Buy() {
+    const {loading, data} = useQuery(QUERY_USERS)
+    const users = data?.users||[]
+    console.log(users)
     return (
         <>
             <BuyingCard />
